9b83cbab30f1a3ca248826ba63702d5c8d67d8d7
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-05-23 09:09:14 +0000

    p11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,20 @@
-# Tuples
-
-filename = "dataset/mbox-short.txt"
+fname=input("Enter the file name: ")
+fh=open(fname)
+counts=dict()
+for line in fh:
+    if line.startswith("From"):
+        words=line.split()
+        n=line.find(":")
+        words=words[n-2:n+1]
+        if words not in counts:
+                counts[words] = 1
+        else:
+                counts[words] += 1
+        counts[words]=int(counts[words])
+max=0
+value=""
+for i in counts:
+    if counts[i]>max:
+        max=counts[i]
+        value=i
+print(value,max)              
