bed6b02347e0e1079ccced24168693a8fcef6221
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-05-25 04:01:00 +0000

    p12

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,12 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+import re
+fname=input("Enter the file name:")
+fh=open(fname)
+sum = 0
+l=list()
+for line in fh:
+    line=line.rstrip()
+    x=re.findall('[0-9]+',line)
+    l+=x
+for i in l:
+    sum+=int(i)
+print(sum)
