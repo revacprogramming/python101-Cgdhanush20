cfd7255f5dfe1897250a0173564e24cb421a9589
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-06-01 03:29:45 +0000

    revised

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -3,7 +3,7 @@ if hrs<= 40:
     fee=hrs*1.5
 elif hrs>40:
     hrs1=hrs-40
-    fee=40*10.5+hrs1*10.5*r
+    fee=40*10.5+hrs1*10.5
 else:
     fee=0
 print(fee) 
---
+++ ActivitySet#1/problem#22.py
@@ -0,0 +1,17 @@
+'''def f(*args):
+  print(args,*args)     
+f(1,2,3,4)
+
+
+def test(a,b,c):
+  print(a,b,c)
+t=(1,2,3)
+test(*t)'''
+
+
+
+'''def f(*args,**kargs):
+  print(args,*args,kargs)
+f(1,2,3,a=21,b=23)'''  
+
+
