09f0389b52dc6c6df32beebb019976829c4f5e60
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-06-02 10:21:13 +0000

    .

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -2,8 +2,7 @@ def computepay(h, r):
     if h<=40:
         return h*r
     elif h>40:
-        h1=h-40
-        fee=40*r+h1*r*1.5
+        fee=40*r+(h-40)*r*1.5
         return fee
 hrs = float(input("Enter hours? "))
 rte = float(input("Enter rate per hour? "))
