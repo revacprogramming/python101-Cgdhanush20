eaee6d56145c01e69b70aa5b7f7e13b7d69a1186
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-06-04 17:21:09 +0000

    Rev

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -2,8 +2,8 @@ hrs = float(input("Enter hours "))
 if hrs<= 40:
     fee=hrs*1.5
 elif hrs>40:
-    hrs1=hrs-40
-    fee=40*10.5+hrs1*10.5
+    hrs2=hrs-40
+    fee=40*10.5+hrs2*10.5
 else:
     fee=0
 print(fee) 
