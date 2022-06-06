b793d341dd7fda9a0917a90cdb30a516d8748f0d
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-06-06 10:33:13 +0000

    p13 completed

---
+++ ActivitySet#1/problem#18.py
@@ -0,0 +1,10 @@
+fname=input('Enter the file name: ')
+fh=open(fname)
+count=0
+for line in fh:
+    if line.startswith("From"):
+        l=line
+        l.split()
+        print(l[1])
+        count += 1
+print(count,'number of times the line started with From')
