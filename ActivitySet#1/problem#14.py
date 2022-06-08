17a97cc7f25839623d6449c94d8185842c085d16
CG DHANUSH <95674712+Cgdhanush20@users.noreply.github.com>
2022-06-08 04:06:43 +0000

    p14 completed

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,19 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url=input('Enter-')
+xml=urllib.request.urlopen(url,context=ctx).read()
+tree=ET.fromstring(xml)
+counts=tree.findall('comments/comment')
+sum=0
+count=0
+for n in counts:
+    t=n.find('count').text
+    sum=sum+int(t)
+    count=count+1
+print('sum=',sum,'\ncount=',count)
